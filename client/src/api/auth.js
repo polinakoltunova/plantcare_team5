5e427a516e7761195bcf60bd787efc0d55a56fd9
Patineshka <gadzieva1@gmail.com>
2026-03-14 13:27:44 +0300

    fix: исправлена авторизация

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,7 +1,25 @@
 import api from "./api";
-export const login = (data) => {
-  return api.post("/auth/login", data);
+
+export const login = async ({ username, password }) => {
+ const formData = new URLSearchParams();
+ formData.append("username", username);
+ formData.append("password", password);
+
+ const response = await api.post("/auth/login", formData, {
+   headers: {
+     "Content-Type": "application/x-www-form-urlencoded",
+   },
+ });
+
+ return response.data;
 };
-export const getCurrentUser = () => {
-  return api.get("/auth/me");
+
+export const getCurrentUser = async () => {
+ const response = await api.get("/auth/me");
+ return response.data;
+};
+
+export const getUsers = async () => {
+ const response = await api.get("/users");
+ return response.data;
 };
